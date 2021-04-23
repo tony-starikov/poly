249c5b2b178811ecbeb08dc78bbd85da6c2e30d3
anton_starikov <tony.starikov.1992@gmail.com>
2021-04-23 17:43:25 +0300

    Add master view and cards for works and artists

---
+++ resources/views/artist-card.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-4">
+    <div class="card mb-4 box-shadow">
+        <img class="card-img-top" src="http://via.placeholder.com/640x360" alt="Card image cap">
+        <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-muted">9 mins</small>
+            </div>
+        </div>
+    </div>
+</div>
