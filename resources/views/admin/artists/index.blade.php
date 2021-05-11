232a4c71a0677f87680d289388e14749aaff10c9
anton_starikov <tony.starikov.1992@gmail.com>
2021-05-11 04:09:59 +0300

    Edit views for main admin artists and software pages

--- resources/views/admin/artists/index.blade.php
+++ resources/views/admin/artists/index.blade.php
@@ -13,18 +13,30 @@
 
         <a class="btn btn-success" type="button" href="{{ route('artists.create') }}">ADD ARTIST</a>
 
-        @foreach($artists as $artist)
-            <p>
-                id: {{ $artist->id }} | name: {{ $artist->name }}
-                <a href="{{ route('artists.show', $artist) }}"><button type="button" class="btn btn-link">SHOW</button></a>
-                <a href="{{ route('artists.edit', $artist) }}"><button type="button" class="btn btn-link">EDIT</button></a>
-            </p>
-            <form id="delete-form" action="{{ route('artists.destroy', $artist) }}" method="POST">
-                @method('DELETE')
-                @csrf
-                <button type="submit" class="btn btn-primary">DELETE</button>
-            </form>
-        @endforeach
+        <hr>
 
+        <div class="row">
+            @foreach($artists as $artist)
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" src="http://via.placeholder.com/640x360" alt="Card image cap">
+                        <div class="card-body">
+                            <p>id: {{ $artist->id }} | name: {{ $artist->name }}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <a href="{{ route('artists.show', $artist) }}"><button type="button" class="btn btn-link">SHOW</button></a>
+                                    <a href="{{ route('artists.edit', $artist) }}"><button type="button" class="btn btn-link">EDIT</button></a>
+                                </div>
+                                <form id="delete-form" action="{{ route('artists.destroy', $artist) }}" method="POST">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit" class="btn btn-primary">DELETE</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div> <!-- /container -->
 @endsection
--- resources/views/admin/software/index.blade.php
+++ resources/views/admin/software/index.blade.php
@@ -13,18 +13,30 @@
 
         <a class="btn btn-success" type="button" href="{{ route('software.create') }}">ADD SOFTWARE</a>
 
-        @foreach($software as $soft)
-            <p>
-                id: {{ $soft->id }} | name: {{ $soft->name }}
-                <a href="{{ route('software.show', $soft) }}"><button type="button" class="btn btn-link">SHOW</button></a>
-                <a href="{{ route('software.edit', $soft) }}"><button type="button" class="btn btn-link">EDIT</button></a>
-            </p>
-            <form id="delete-form" action="{{ route('software.destroy', $soft) }}" method="POST">
-                @method('DELETE')
-                @csrf
-                <button type="submit" class="btn btn-primary">DELETE</button>
-            </form>
-        @endforeach
+        <hr>
 
+        <div class="row ">
+            @foreach($software as $soft)
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" src="http://via.placeholder.com/640x360" alt="Card image cap">
+                        <div class="card-body">
+                            <p>id: {{ $soft->id }} | name: {{ $soft->name }}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <a href="{{ route('software.show', $soft) }}"><button type="button" class="btn btn-link">SHOW</button></a>
+                                    <a href="{{ route('software.edit', $soft) }}"><button type="button" class="btn btn-link">EDIT</button></a>
+                                </div>
+                                <form id="delete-form" action="{{ route('software.destroy', $soft) }}" method="POST">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit" class="btn btn-primary">DELETE</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div> <!-- /container -->
 @endsection
